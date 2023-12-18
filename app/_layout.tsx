04498425637d418dcee6b1f8eaaa63095d0ee417
avengsum <sy710481@gmail.com>
2023-12-19 03:52:18 +0530

    clerk added

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,13 @@
 import { Ionicons } from '@expo/vector-icons';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
+import { isLoaded, useFonts } from 'expo-font';
 import { SplashScreen, Stack, router, useRouter } from 'expo-router';
 import { useEffect } from 'react';
 import { TouchableOpacity, useColorScheme } from 'react-native';
+import * as SecureStore from 'expo-secure-store'
+import { ClerkProvider, useAuth } from '@clerk/clerk-expo';
+
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -24,7 +27,9 @@ export default function RootLayout() {
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
     ...FontAwesome.font,
   });
+  
 
+  
   // Expo Router uses Error Boundaries to catch errors in the navigation tree.
   useEffect(() => {
     if (error) throw error;
@@ -40,12 +45,44 @@ export default function RootLayout() {
     return null;
   }
 
-  return <RootLayoutNav />;
+  const CLERK_PUBLISHABLE_KEY = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY
+
+  const tokenCache = {
+    async getToken(key:string){
+      try{
+        return SecureStore.getItemAsync(key);
+            } catch(err){
+              return null;
+            }
+    },
+    async saveToken(key:string,value:string) {
+      try {
+        return SecureStore.setItemAsync(key,value);
+      }
+      catch(err){
+        return;
+      }
+    }
+  }
+
+  return(
+    <ClerkProvider publishableKey={CLERK_PUBLISHABLE_KEY!} tokenCache={tokenCache} >
+      <RootLayoutNav />;
+    </ClerkProvider>
+  )
+  
 }
 
 function RootLayoutNav() {
   const colorScheme = useColorScheme();
   const router = useRouter();
+  const {isLoaded , isSignedIn} = useAuth();
+
+  useEffect(() => {
+    if (isLoaded && !isSignedIn) {
+      router.push('/(modals)/login');
+    }
+  },[isLoaded]);
 
   return (
    
@@ -74,6 +111,7 @@ function RootLayoutNav() {
           ),
         }}
         />
+
       </Stack>
   );
 }
