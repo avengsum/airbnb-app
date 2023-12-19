6dfb333f0fb9dac800abd1d47826017cdb239ed3
avengsum <sy710481@gmail.com>
2023-12-19 12:04:53 +0530

    authentication implemented

---
+++ app/(modals)/login.tsx
@@ -0,0 +1,114 @@
+import { View, Text, TextInput,  StyleSheet,TouchableOpacity } from 'react-native'
+import React from 'react'
+import { defaultStyles } from '@/constants/Styles'
+import Colors from '@/constants/Colors';
+import { Ionicons } from '@expo/vector-icons';
+import { useOAuth } from '@clerk/clerk-expo';
+import { useRouter } from 'expo-router';
+
+enum Strategy {
+  Google='oauth_google',
+  Apple='oauth_apple',
+  Facebook='oauth_facebook',
+}
+
+const login = () => {
+
+  const router = useRouter();
+
+  const {startOAuthFlow:googleAuth} = useOAuth({strategy:'oauth_google'})
+  const {startOAuthFlow:appleAuth} = useOAuth({strategy:'oauth_apple'})
+  const {startOAuthFlow:facebookAuth} = useOAuth({strategy:'oauth_facebook'})
+
+  const onSelectedAuth = async (strategy:Strategy) => {
+    const selectedAuth = {
+      [Strategy.Google]:googleAuth,
+      [Strategy.Facebook]:facebookAuth,
+      [Strategy.Apple]:appleAuth
+    }[strategy];
+
+    try{
+      const {createdSessionId,setActive} = await selectedAuth();
+      console.log(createdSessionId)
+
+      if(createdSessionId){
+        setActive!({session:createdSessionId});
+        router.back();
+      }  
+    } catch(err) {
+      console.log("OAuth Error: ",err)
+    }
+  }
+
+  return (
+    <View>
+      <TextInput autoCapitalize='none' placeholder='Email' style ={[defaultStyles.inputField,{marginBottom:30}]} />
+      <TouchableOpacity style={defaultStyles.btn} >
+        <Text style={defaultStyles.btn}>Continue</Text>
+      </TouchableOpacity>
+      <View style={styles.separatorView}>
+        <View style={{
+          borderBottomColor:'#000',
+          borderBottomWidth:StyleSheet.hairlineWidth,
+        }} />
+          <Text style={styles.separtor}>or</Text>
+          <View style={{
+          borderBottomColor:'#000',
+          borderBottomWidth:StyleSheet.hairlineWidth,
+        }} />
+      </View>
+      <View style={{gap:20}}>
+        <TouchableOpacity style={styles.btnOutline} >
+          <Ionicons name='call-outline' size={24} style={defaultStyles.btnIcon} />
+          <Text style={styles.btnOutlineText}>Continue with Phone</Text>
+        </TouchableOpacity> 
+        <TouchableOpacity style={styles.btnOutline} onPress={() => onSelectedAuth(Strategy.Apple)}>
+          <Ionicons name='md-logo-apple' size={24} style={defaultStyles.btnIcon} />
+          <Text style={styles.btnOutlineText}>Continue with Apple</Text>
+        </TouchableOpacity> 
+        <TouchableOpacity style={styles.btnOutline} onPress={() => onSelectedAuth(Strategy.Google)}>
+          <Ionicons name='md-logo-google' size={24} style={defaultStyles.btnIcon} />
+          <Text style={styles.btnOutlineText}>Continue with Google</Text>
+        </TouchableOpacity> 
+        <TouchableOpacity style={styles.btnOutline} onPress={() => onSelectedAuth(Strategy.Facebook)}>
+          <Ionicons name='md-logo-facebook' size={24} style={defaultStyles.btnIcon} />
+          <Text style={styles.btnOutlineText}>Continue with Facebook</Text>
+        </TouchableOpacity> 
+      </View>
+    </View>
+  )
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex:1,
+    backgroundColor:'#fff',
+    padding:26,
+  },
+  separatorView: {
+    flexDirection:'row',
+    gap:10,
+    alignItems:'center',
+    marginVertical:30,
+  },
+  separtor: {
+    color:Colors.grey
+  },
+  btnOutline: {
+    backgroundColor:'#fff',
+    borderWidth:1,
+    borderColor:Colors.grey,
+    height:50,
+    borderRadius:8,
+    alignItems: 'center',
+    justifyContent:'center',
+    flexDirection:'row',
+    paddingHorizontal:10,
+  },
+  btnOutlineText: {
+    color:'#000',
+    fontSize:16,
+  }
+})
+
+export default login
